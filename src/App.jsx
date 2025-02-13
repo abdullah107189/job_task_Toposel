6b249e2d2af7baf80a7af0891082e3c87c99f04b
abdullah107189 <abdullah107189@gmail.com>
2025-02-13 23:35:07 +0600

    set up react route and set up other componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./componets/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="text-blue-400">hello</div>;
+  return (
+    <div className="text-blue-400">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    
+    <RouterProvider router={router} />
   </StrictMode>
 );
